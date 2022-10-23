4010271df17634b6a342c8a016b3f59471f98e2a
qckpea <csortosnorbert@gmail.com>
2022-10-23 13:46:50 +0200

    display surface normals of the sphere

--- main.cpp
+++ main.cpp
@@ -17,28 +17,54 @@ struct ray {
     }
 };
 
-bool32 HitSphere(point3 center, r32 radius, ray r)
+r32 HitSphere(point3 center, r32 radius, ray r)
 {
+    // analytic solution
+    // O^2+(Dt)^2+2ODt−R^2 = 0
+    // O^2+D^2t^2+2ODt−R^2 = 0
+    // f(x)=ax^2+bx+c
+    // a = dot(D, D)            <= {D^2}
+    // b = 2 * dot(O, D)        <= {2OD}
+    // c = dot(O, O) - R * R    <= {O^2 - R^2}
     v3 centerToOrigin = r.orig - center;
     r32 a = DotProduct(r.dir, r.dir);
     r32 b = 2.0f * DotProduct(centerToOrigin, r.dir);
     r32 c = DotProduct(centerToOrigin, centerToOrigin) - radius * radius;
     
     r32 discriminant = b*b - 4*a*c;
-    return discriminant > 0;
+
+    if (discriminant < 0)
+    {
+        return -1.0f;
+    }
+    else
+    {
+        return (-b - SquareRoot(discriminant) ) / (2.0*a);
+    }
 }
 
-v3 RayColor(const ray& r) {
-    if(HitSphere(point3{0.0f, 0.0f, -1.0f}, 0.5f, r))
+v3 RayColor(ray& r) {
+    point3 sphereCenter = {0.0f, 0.0f, -1.0f};
+    r32 radius = 0.5f;
+    r32 t = HitSphere(sphereCenter, radius, r);
+    if(t > 0.0f)
     {
-        return {0.0f, 1.0f, 0.0f};
+        v3 normal = Normalize(r.at(t) - sphereCenter);
+#if 1
+        return 0.5 * v3{normal.x + 1.0f, normal.y + 1.0f, normal.z + 1.0f};
+#else
+        // testing the colors
+        return 0.5 * v3{normal.x, normal.y, normal.z};
+#endif
     }
+
 #if 1
     v3 unit_direction = Normalize(r.dir);
 #else
     v3 unit_direction = r.dir;
 #endif
-    r32 t = 0.5f * (unit_direction.y + 1.0f);
+    
+    t = 0.5f * (unit_direction.y + 1.0f);
     v3 bg = {1.0f, 1.0f, 1.0f};
     v3 fg = {1.0f, 0.0f, 0.0f};
     return (1.0f - t) * bg + t * fg;
@@ -83,9 +109,13 @@ int main() {
         printf("Raytracing...\n");
         for(s32 j = IMG_HEIGHT - 1;
             j >= 0;
-            --j)
+            j--)
         {
-            printf("\rScanlines remaining:%d", j);
+            if (j % 64 == 0)
+            {
+                printf("\rScanlines remaining:%d, progress: %d%%", j, 100 * (IMG_HEIGHT-j) / IMG_HEIGHT );
+            }
+
             for(s32 i = 0;
                 i < IMG_WIDTH;
                 i++)
